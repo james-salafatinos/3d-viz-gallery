c3e4bae3ee8fd13e0e8ed4b5d3711620745e7ea4
Glitch (hello-express) <none>
2021-08-01 16:23:04 +0000

    👕🛢 Checkpoint ./public/client.js:34433185/7969

--- public/client.js
+++ public/client.js
@@ -5,33 +5,32 @@
 // import { DirectionalLightHelper } from 'three'
 // import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js'
 
-import * as THREE from 'https://cdn.skypack.dev/three';
-import { OrbitControls }from 'https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls'
+import * as THREE from "https://cdn.skypack.dev/three";
+import { OrbitControls } from "https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls";
 
-console.log("HELLO", THREE, OrbitControls)
+console.log("HELLO", THREE, OrbitControls);
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 import { lights } from "./_lights.js";
-Object.entries(lights).forEach((element) => {
+Object.entries(lights).forEach(element => {
   scene.add(element[1]);
 });
 
 import { items } from "./_items.js";
-Object.entries(items).forEach((element) => {
-  console.log(element[1])
+Object.entries(items).forEach(element => {
+  console.log(element[1]);
   scene.add(element[1]);
 });
-
 
 var V = [];
 var stack = [];
 var score = 0;
 
-let distributeStars = function () {
+let distributeStars = function() {
   const vertices = [];
   for (let i = 0; i < 20; i++) {
     const x = THREE.MathUtils.randFloatSpread(200);
@@ -55,21 +54,17 @@ let distributeStars = function () {
   return points;
 };
 
+const stars = distributeStars();
+scene.add(stars);
 
-
-
-
-
-let nextStars = function(vertices){
-  let newVertices = []
+let nextStars = function(vertices) {
+  let newVertices = [];
   for (let i = 0; i < vertices.length; i++) {
-    const dx = THREE.MathUtils.randFloatSpread(2)
-    newVertices.push(vertices[i]+ dx)
-    V[i] = vertices[i] + dx
-    
-   
+    const dx = THREE.MathUtils.randFloatSpread(2);
+    newVertices.push(vertices[i] + dx);
+    V[i] = vertices[i] + dx;
   }
-const geometry = new THREE.BufferGeometry();
+  const geometry = new THREE.BufferGeometry();
   geometry.setAttribute(
     "position",
     new THREE.Float32BufferAttribute(newVertices, 3)
@@ -78,81 +73,82 @@ const geometry = new THREE.BufferGeometry();
   const material = new THREE.PointsMaterial({ color });
 
   const points = new THREE.Points(geometry, material);
-
   return points;
-}
-
-
+};
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  width: window.innerWidth,
+  height: window.innerHeight
+};
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
-camera.position.x = 1
-camera.position.y = 1
-camera.position.z = 2
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
 
 // Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  canvas: canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+var animate = function() {
+  const elapsedTime = clock.getElapsedTime();
 
-    // Update controls
-    controls.update()
-  
-    console.log(nextStars(V))
-    scene.add(nextStars(V))
-    // stack.push(1)
+  // Update controls
+  controls.update();
 
+  // console.log(nextStars(V));
+  scene.add(nextStars(V));
+  stack.push(1);
+  console.log("scene", scene.children[scene.children.length-1].geometry)
+  let lastPoints = scene.children[scene.children.length-1]
+  lastPoints.geometry.dispose()
 
+  // Render
+  renderer.render(scene, camera);
 
-    // Render
-    renderer.render(scene, camera)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(animate);
+};
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
+animate();
